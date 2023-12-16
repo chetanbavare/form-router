dd8548c3de13d0a14b7079a52f52bdc6cfae32a3
chetanbavare <chetsmartk@gmail.com>
2023-12-16 15:42:00 +0530

    fifth commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,17 @@
-
+import Template from "../components/Template";
+import loginImg from "../assets/login.png"
 
 function Login({ setIsLoggedIn }) {
-  
+  return (
+    <Template 
+        title = "Welcome Back"
+        desc1 = "Build skills for today, tomorrow, and beyond."
+        desc2 = "Education to future-proof your career."
+        formtype = "login"
+        image = {loginImg}
+        setIsLoggedIn = {setIsLoggedIn}
+    />
+  )
 }
 
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,18 @@
+import Template from "../components/Template";
+import signImg from "../assets/signup.png"
 
 function Signup({ setIsLoggedIn }) {
-  
+    return (
+        <Template
+        title = "Join the millions learning to code with StudyNotion for free"
+        desc1 = "Build skills for today, tomorrow, and beyond."
+        desc2 = "Education to future-proof your career."
+        formtype = "signup"
+        image = {signImg}
+        setIsLoggedIn = {setIsLoggedIn}
+    />
+    )
+   
 }
 
 export default Signup;
